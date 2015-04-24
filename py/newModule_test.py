7bd1681800bd6e2ef4bc7e762bde7d3153a944ed
7yl4r <murray.tylar@gmail.com>
2015-04-24 03:25:26 -0400

    added test for package.json change on new (currently failing)

--- py/newModule_test.py
+++ py/newModule_test.py
@@ -4,22 +4,28 @@ import shutil
 import os.path
 
 import CONFIG
+import utils
 import newModule
+
+TEST_NAME = "temp"  # fake module name to use for testing (should be lowerCamelCase)
 
 
 class TestNewModule(unittest.TestCase):
 
     def setUp(self):
-        newModule.create_module("temp")
+        newModule.create_module(TEST_NAME)
 
     def tearDown(self):
-        shutil.rmtree(CONFIG.module_dir+'/temp')
+        shutil.rmtree(CONFIG.module_dir+'/'+TEST_NAME)
 
     def test_that_requested_files_exists(self):
         """tests that directory is created"""
-        self.assertTrue(os.path.isfile(CONFIG.module_dir+'/temp/temp.html'))
-        self.assertTrue(os.path.isfile(CONFIG.module_dir+'/temp/temp.less'))
+        self.assertTrue(os.path.isfile(CONFIG.module_dir+'/'+TEST_NAME+'/'+TEST_NAME+'.html'))
+        self.assertTrue(os.path.isfile(CONFIG.module_dir+'/'+TEST_NAME+'/'+TEST_NAME+'.less'))
 
+    def test_that_dep_added_to_package_json(self):
+        data = utils.get_package_json()
+        self.assertIsNotNone(data['browser'][utils.get_hyphen_name(TEST_NAME)])
     
 if __name__ == '__main__':
     unittest.main()
